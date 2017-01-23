4241d2b54726f3300eecbbe38a98d12e72423d03
Nicolas De Neuter <nico.d.n@hotmail.com>
2017-01-23 18:00:26 +0100

    Updated readcountclass.py script for running with bash and fixed bugs.
    - Added the option to run the readcountclass script using arguments to allow its use in different workflows. 
    - Moved functions in front of classes because rct class requires function to work.
    - Fixed a few buggy references where 'genename' was still used instead of referencing the rct instanc's gene_name_column attribute.

--- readcountclass.py
+++ readcountclass.py
@@ -5,12 +5,73 @@ import natsort
 from numpy import mean
 
 
+def concat_read_count_tables(listoftables):
+    
+    ''' Given multiple read count table instances, combine them into a single instance.
+    List of genes in the final table contains all the genes present in different read count tables.
+    If a read count table didn't have any data on a gene present in another table,
+    it's samples are each assigned a value of 0 for that gene. '''
+    
+    # concat all rcts
+    # always set column containing gene names as index
+    total_rct = listoftables[0].set_index(listoftables[0].gene_column_name)
+    for i in range(len(listoftables)-1):
+        rct_to_add = listoftables[i+1].set_index(listoftables[i+1].gene_column_name)
+        total_rct = pd.concat([total_rct, rct_to_add], axis=1)
+    
+    # replace missing values with 0
+    total_rct = total_rct.fillna(0)
+    
+    # set genename as a column instead of index
+    total_rct[total_rct.gene_column_name] = total_rct.index
+
+    return ReadCountTable(total_rct)
+
+
+def process_nonresp_0(columnname):
+    
+    ''' Processes a sample's name into the different factors associated with the sample.
+    To be used during the writing of coldata (ie: factor table) with the
+    write_DESeq2_files method of the ReadCountTable class. '''
+     
+    ind = columnname.split('/')[-1].split('_')[0]
+    resp = response_dict[ind]
+    if resp == 'Non-resp':
+        day = 0
+    elif resp == 'Resp':
+        day = columnname.split('/')[-1].split('_')[1].replace('EXP', '')
+    rep = columnname.split('/')[-1].split('_')[2]
+    run = columnname.split('/')[-3].split('_')[0]
+    
+    return ind, day, rep, run, resp
+
+
+def make_responders_dict(datafile = "/Users/nicolasdeneuter/Dropbox/GOA/HepB run/Hep B run incl test _ Samples overview.xlsx"):
+    
+    ''' Process data on volunteers to determine if they're responders or not.
+    Return a dict with volunteers as keys and their response as key. '''
+    
+    # read in data
+    all_data = pd.read_excel(datafile, skiprows = [2, 3, 4, 5], header = [0, 1], sheetname = 'ELISA', index_col = 0)
+    # select subset of data on elisa results for day 60
+    elisa_data = all_data['Anti-HBS (IU/L)'][['ELISA_60 (Batch)', 'ELISA_60 (Repeat)']]
+    # drop patients with missing values
+    elisa_data = elisa_data.dropna()
+    # change everything to numbers (< 2 => 0 and > 1000 => 1000)
+    elisa_data = elisa_data.applymap(lambda x: x if x != '< 2' else 0)
+    elisa_data = elisa_data.applymap(lambda x: x if x != '>1000' else 1000)
+    # set each patient to responder (True) or non-responder (False) and turn series to dict after calculating mean response
+    elisa_data = dict(elisa_data.apply(mean, axis = 1).map(lambda x: 'Non-resp' if x <= 10 else 'Resp'))
+    
+    return elisa_data
+
+
 class ReadCountTable(pd.DataFrame):
     
     def __init__(self, df, gene_column_name = 'genename'):
         
         super().__init__(df)
-        self.__gene_column_name = gene_column_name
+        self.gene_column_name = gene_column_name
         for columnname in self.columns:
             if 'Unnamed' in columnname:
                 del self[columnname]
@@ -41,7 +102,7 @@ class ReadCountTable(pd.DataFrame):
         for columnname in self.columns:
             
             # skip irrelevant columns for grouping
-            if columnname == self.__gene_column_name:
+            if columnname == self.gene_column_name:
                 continue
             # make common name for lanegroup
             sample = '_'.join(columnname.split('_')[:-3])
@@ -75,7 +136,7 @@ class ReadCountTable(pd.DataFrame):
         4) processor: function that processes sample names to extract factors from it; is specific to analysis/dataset. '''
                 
         # put gene name column first! DESeq2 analysis cares about order of columns
-        resorted_cols = [self.__gene_column_name]+natsort.natsorted(list([x for x in self.columns if x != self.__gene_column_name]), key=lambda x:x.split('/')[-1])
+        resorted_cols = [self.gene_column_name]+natsort.natsorted(list([x for x in self.columns if x != self.gene_column_name]), key=lambda x:x.split('/')[-1])
         self = self[resorted_cols]
         
         # write count table
@@ -89,112 +150,60 @@ class ReadCountTable(pd.DataFrame):
             if processor == process_nonresp_0:
                 f.write('Ind\tDay\tRepeat\tRun\tResp\n')
                 for columnname in self.columns:
-                    if columnname != 'genename':
+                    if columnname != self.gene_column_name:
                         ind, day, rep, run, resp = process_nonresp_0(columnname)
                         f.write('{}\t{}\t{}\t{}\t{}\n'.format(ind, day, rep, run, resp))
         
 
-def make_responders_dict(datafile = "/Users/nicolasdeneuter/Dropbox/GOA/HepB run/Hep B run incl test _ Samples overview.xlsx"):
-    
-    ''' Process data on volunteers to determine if they're responders or not.
-    Return a dict with volunteers as keys and their respons as key. '''
-    
-    # read in data
-    all_data = pd.read_excel(datafile, skiprows = [2, 3, 4, 5], header = [0, 1], sheetname = 'ELISA', index_col = 0)
-    # select subset of data on elisa results for day 60
-    elisa_data = all_data['Anti-HBS (IU/L)'][['ELISA_60 (Batch)', 'ELISA_60 (Repeat)']]
-    # drop patients with missing values
-    elisa_data = elisa_data.dropna()
-    # change everything to numbers (< 2 => 0 and > 1000 => 1000)
-    elisa_data = elisa_data.applymap(lambda x: x if x != '< 2' else 0)
-    elisa_data = elisa_data.applymap(lambda x: x if x != '>1000' else 1000)
-    # set each patient to responder (True) or non-responder (False) and turn series to dict after calculating mean response
-    elisa_data = dict(elisa_data.apply(mean, axis = 1).map(lambda x: 'Non-resp' if x <= 10 else 'Resp'))
-    
-    return elisa_data
-
-
-def concat_read_count_tables(listoftables):
-    
-    ''' Given multiple read count table instances, combine them into a single instance.
-    List of genes in the final table contains all the genes present in different read count tables.
-    If a read count table didn't have any data on a gene present in another table,
-    it's samples are each assigned a value of 0 for that gene. '''
-    
-    # concat all rcts
-    # always set column containing gene names as index
-    total_rct = listoftables[0].set_index(listoftables[0].__gene_column_name)
-    for i in range(len(listoftables)-1):
-        rct_to_add = listoftables[i+1].set_index(listoftables[i+1].__gene_column_name)
-        total_rct = pd.concat([total_rct, rct_to_add], axis=1)
-        
-    # replace missing values with 0
-    total_rct = total_rct.fillna(0)
-    
-    # set genename as a column instead of index
-    total_rct['genename'] = total_rct.index
-
-    return ReadCountTable(total_rct)
-
-
-def process_nonresp_0(columnname):
-    
-    ''' Processes a sample's name into the different factors associated with the sample.
-    To be used during the writing of coldata (ie: factor table) with the
-    write_DESeq2_files method of the ReadCountTable class. '''
-     
-    ind = columnname.split('/')[-1].split('_')[0]
-    resp = response_dict[ind]
-    if resp == 'Non-resp':
-        day = 0
-    elif resp == 'Resp':
-        day = columnname.split('/')[-1].split('_')[1].replace('EXP', '')
-    rep = columnname.split('/')[-1].split('_')[2]
-    run = columnname.split('/')[-3].split('_')[0]
-    
-    return ind, day, rep, run, resp
-
-
 
 if __name__ == '__main__':
     
-    from glob import glob
-    import random
+    import os
     import sys
+    from glob import glob
     
-    # data directories
-    indir = "/Users/nicolasdeneuter/Bestanden/PhD/Projects/GOA/RNAseq/readcounts"
-    outdir = "/Users/nicolasdeneuter/Bestanden/PhD/Projects/GOA/RNAseq/readcounts"
+    mode = sys.argv[1]
     
-    # read each count table in indir and save it as count table instance
-    rct_list = []
-    for filepath in glob(indir+'/*'):
-        if 'readcount' in filepath.split('/')[-1]:
-            rct = ReadCountTable(pd.read_csv(filepath, sep = '\t'))
-            # combine counts for same sample over different lanes
-            rct.combine_lane_counts()
-            rct_list.append(rct)
+    if mode == '-R':
     
-    # combine all count tables
-    total_rct = concat_read_count_tables(rct_list)
-    
-    response_dict = make_responders_dict()
-    # remove samples for which responder status is unknown
-    for sample in total_rct.columns:
-        if sample.split('/')[-1].split('_')[0] not in response_dict.keys() and sample != total_rct.__gene_column_name:
-            del total_rct[sample]
-                    
-    # remove outlier sample 
-    total_rct.remove_sample('H1_EXP0_1')
-    
-    # H6_EXP3_1 was sequenced twice due to bad quality, take mean of the two runs since they're both of lower quality
-    total_rct['/home/shared_data_immuno/Run_Data/161125+170111_NB501809_0047_AH2HH2BGX2/tmp_files/H6_EXP3_1'] = \
-        total_rct[['/home/shared_data_immuno/Run_Data/170111_NB501809_0047_AH2HH2BGX2/tmp_files/H6_EXP3_1_S29',\
-                    '/home/shared_data_immuno/Run_Data/161125_NB501809_0023_AHLC7CBGXY/tmp_files/H6_EXP3_1_S15']].sum(axis =1)
-    del total_rct['/home/shared_data_immuno/Run_Data/170111_NB501809_0047_AH2HH2BGX2/tmp_files/H6_EXP3_1_S29']
-    del total_rct['/home/shared_data_immuno/Run_Data/161125_NB501809_0023_AHLC7CBGXY/tmp_files/H6_EXP3_1_S15']
-    
-    # write data to files
-    total_rct.write_DESeq2_files(outdir)
-    
-    
+        # read and write data to working directory
+        # curdir = os.getcwd()
+        curdir = "/Users/nicolasdeneuter/Bestanden/PhD/Projects/GOA/RNAseq/readcounts"
+        
+        # read each count table in curdir and save it as count table instance
+        rct_list = []
+        for filepath in glob(curdir+'/*'):
+            if 'readcount' in filepath.split('/')[-1]:
+                rct = ReadCountTable(pd.read_csv(filepath, sep = '\t'))
+                # combine counts for same sample over different lanes
+                rct.combine_lane_counts()
+                rct_list.append(rct)
+        
+        if len(rct_list) == 0:
+            raise IOError('No read count files were found.')
+        
+        # combine all count tables
+        total_rct = concat_read_count_tables(rct_list)
+        
+        response_dict = make_responders_dict()
+        # remove samples for which responder status is unknown
+        for sample in total_rct.columns:
+            if sample.split('/')[-1].split('_')[0] not in response_dict.keys() and sample != total_rct.gene_column_name:
+                del total_rct[sample]
+                        
+        # remove outlier sample 
+        total_rct.remove_sample('H1_EXP0_1')
+        
+        # H6_EXP3_1 was sequenced twice due to bad quality, take mean of the two runs since they're both of lower quality
+        total_rct['/home/shared_data_immuno/Run_Data/161125+170111_NB501809_0047_AH2HH2BGX2/tmp_files/H6_EXP3_1'] = \
+            total_rct[['/home/shared_data_immuno/Run_Data/170111_NB501809_0047_AH2HH2BGX2/tmp_files/H6_EXP3_1_S29',\
+                        '/home/shared_data_immuno/Run_Data/161125_NB501809_0023_AHLC7CBGXY/tmp_files/H6_EXP3_1_S15']].sum(axis =1)
+        del total_rct['/home/shared_data_immuno/Run_Data/170111_NB501809_0047_AH2HH2BGX2/tmp_files/H6_EXP3_1_S29']
+        del total_rct['/home/shared_data_immuno/Run_Data/161125_NB501809_0023_AHLC7CBGXY/tmp_files/H6_EXP3_1_S15']
+        
+        # write data to files
+        total_rct.write_DESeq2_files(curdir)
+        
+    elif mode == '-ML':
+        
+        pass
