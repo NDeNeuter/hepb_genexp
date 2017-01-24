78ea1f404238a9153c841f5183391649f7872420
Nicolas De Neuter <nico.d.n@hotmail.com>
2017-01-24 15:06:00 +0100

    Fixed typo in R pipeline bash script.

--- DE_genexpression_R.sh
+++ DE_genexpression_R.sh
@@ -2,9 +2,9 @@
 
 cd "$(dirname "$0")"
 
+echo "Running R based pipeline"
+
 # do data preparation for DESeq2 analysis in R
-## If running on other pc - make sure to check curdir used by python script
-## and make sure that all data files are in the dir as the bash script
 python3 read_count_class.py -R
 
 # do DESeq2 analysis using all volunteers
