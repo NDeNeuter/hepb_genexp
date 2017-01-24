d8fc26168e26f03c367a228bbcbaba7c8bd35447
Nicolas De Neuter <nico.d.n@hotmail.com>
2017-01-24 12:58:32 +0100

    Finished bash script that runs complete R-based analysis.

--- DE_genexpression_R.sh
+++ DE_genexpression_R.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# Write bash script that runs complete analysis of GOA gene-expression data using the full DESeq tactic.
-# Approach tactic: give all non-responders day 0 as day factor.
-
 cd "$(dirname "$0")"
 
-# python does data preparation for DESeq2 analysis in R
-python3 readcountclass.py -R
+# do data preparation for DESeq2 analysis in R
+## If running on other pc - make sure to check curdir used by python script
+## and make sure that all data files are in the dir as the bash script
+python3 read_count_class.py -R
 
-# does DESeq2 analysis using all volunteers
+# do DESeq2 analysis using all volunteers
 # non-responders are all assigned factors as if they have been measured on day 0
-# Rscript deseq2_NDN.R
+Rscript deseq2_NDN.R
 
+rm Rplots.pdf
