7e051cee7a84dac975eda7f4075ce0e11db30a8e
pmeysman <pieter.meysman@gmail.com>
2022-04-07 15:55:16 +0200

    Update to work with latest R, added RData classifier

--- predictor_scripts/classifier.R
+++ predictor_scripts/classifier.R
@@ -1,22 +1,21 @@
 
 # This is the count table, normalized and only for the day 0 samples
 table <- data.frame(read.table('day0_normcounts.txt',header=T,row.names=1))
+table <- table[,]
 
 # Compress features with PCA
 pcares<-prcomp(table[,-13596],center=TRUE,scale=TRUE)
 
+# Write PCA loadings to file
 #for (i in (1:10)){
 #  write.table(colnames(table)[pcares$rotation[,i] < -0.01], file = paste0("predictor_output/PC",i,"_-001.txt",collapse = NULL))
 #}
 
 
-#Naive Bayes
+# Data set for Naive Bayes classifier
 
-data <- data.frame(pcares$x[,1:10],table$Response)
-colnames(data)[11] <- "Response"
-data$Response <- factor(data$Response)
-levels(data$Response) <- make.names(levels(factor(data$Response)))
-
+data <- data.frame(pcares$x[,1:5])
+data["Response"] = as.factor(table$Response)
 
 #Naive Bayes
 
@@ -40,52 +39,53 @@ perf <- performance(rocpred, measure = "tpr", x.measure = "fpr")
 plot(perf) 
 abline(0,1)
 performance(rocpred,measure="auc")@y.values[[1]]
-# AUCROC = 0.661654
+# AUCROC = 0.6857143 (in R 4.1.1)
 
+# PR curve
 perfPR <- performance(rocpred, measure = "prec", x.measure = "rec")
 plot(perfPR) 
 
+# Confusion matrix
 predfit<-values>0.5
 table(predfit,data$Response)
 
+# Accuracy
 performance(rocpred, measure = "acc")@y.values[[1]]
 
 fit<-NaiveBayes(Response ~ ., data=data, usekernel=TRUE)
-plot(fit)
+#plot(fit)
 
 # With the granulocytes
 
 cellcount <- read.table('../data/cellcounts.txt',header=T,row.names=1,sep="\t")
 total <- merge(cellcount,table[,13596, drop=FALSE],by="row.names",all.x=FALSE)
 
-data <- data.frame(pcares$x[,1:10],total$GRA0,table$Response)
-colnames(data)[12] <- "Response"
-data$Response <- factor(data$Response)
-levels(data$Response) <- make.names(levels(factor(data$Response)))
+data.gr <- data.frame(pcares$x[,1:5],total$GRA0)
+data.gr["Response"] = as.factor(table$Response)
 
 #Naive Bayes
-values <- matrix(0,34,1)
+values.gr <- matrix(0,34,1)
 
 require('klaR')
 for(i in 1:34){
-  fit<-NaiveBayes(Response ~ total.GRA0, data=data[-i,], usekernel=FALSE, prior=c(20/34,14/34))
+  fit<-NaiveBayes(Response ~ ., data=data.gr[-i,], usekernel=TRUE, prior=c(20/34,14/34))
   #klaR::plot(fit)
-  pred<-predict(fit, newdata=data[i,])
-  values[i]<-pred$posterior[,2]
+  pred<-predict(fit, newdata=data.gr[i,])
+  values.gr[i]<-pred$posterior[,2]
 }
 require("ROCR")
-rocpred <- prediction(predictions = values, labels=data$Response)
-perf <- performance(rocpred, measure = "tpr", x.measure = "fpr")
-plot(perf) 
+rocpred.gr <- prediction(predictions = values.gr, labels=data.gr$Response)
+perf.gr <- performance(rocpred.gr, measure = "tpr", x.measure = "fpr")
+plot(perf.gr) 
 abline(0,1)
-performance(rocpred,measure="auc")@y.values[[1]]
-#AUCROC = 0.6954887
+performance(rocpred.gr,measure="auc")@y.values[[1]]
+#AUCROC = 0.7178571 (in R 4.1.1)
 
-perfPR <- performance(rocpred, measure = "prec", x.measure = "rec")
-plot(perfPR) 
+perfPR.gr <- performance(rocpred.gr, measure = "prec", x.measure = "rec")
+plot(perfPR.gr) 
 
-predfit<-values>0.5
-table(predfit,data$Response)
+predfit.gr<-values.gr>0.5
+table(predfit.gr,data.gr$Response)
 
-performance(rocpred, measure = "acc")@y.values[[1]]
+performance(rocpred.gr, measure = "acc")@y.values[[1]]
 
